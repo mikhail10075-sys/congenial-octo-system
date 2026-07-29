0f1d37eb0ad4d73e3eecb997f34d163abcddbefa
mikhail10075-sys <mikhail10075@gmail.com>
2026-07-29 11:40:09 +0530

    Add files via upload

--- Test1.py
+++ Test1.py
@@ -1,2 +1,6 @@
-students = {name : 'Roy' , score: 78 , name: 'Ben', score:89, name:'Helen', score:95,name:'Marie', score:76,name:'Lucas',score:75}
+students : {id1:{'name': 'John', 'score': 85},
+id2:{'name': 'Marie', 'score' : 64},
+id3:{'name':'Helen', 'score':97},
+id4:{'name':'Luke', 'score': 82},
+id5:{'name':'Joshua', 'score': 81}}
 
